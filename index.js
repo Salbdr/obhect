2db87094d8665f5f8d631425c2fc057afe37d12e
sult <168359467+Salbdr@users.noreply.github.com>
2024-05-25 23:11:47 +0300

    some change

---
+++ index.js
@@ -0,0 +1,108 @@
+characters = [
+    {
+      name: "Luke Skywalker",
+      height: 177,
+      gender: "male",
+      mass: 77,
+      eye_color: "brown",
+    },
+    {
+      name: "Leia Organa",
+      height: 160,
+      gender: "female",
+      mass: 56,
+      eye_color: "blue",
+    },
+    {
+      name: "Han Solo",
+      height: 180,
+      gender: "male",
+      mass: 80,
+      eye_color: "brown",
+    },
+    {
+      name: "Chewie",
+      height: 222,
+      gender: "male",
+      mass: 190,
+      eye_color: "black",
+    },
+   
+    {
+        name: "kevien",
+        height: 106,
+        gender: "male",
+        mass: 32.2,
+        eye_color: "red",
+      },
+    
+    
+  ];
+
+  //1
+
+
+
+// const blueEyes = characters.filter(e => e.eye_color =="blue")
+// console.log(blueEyes[0].name);
+// blueEyes.forEach(element => { 
+//   console.log(element.name)});
+
+//2
+
+// var gen=characters.filter(x => x.mass>50);
+// console.log(gen[0].gender);
+
+3
+
+// var Height=characters.filter(x => x.height<200);
+// console.log(Height);
+
+//4
+
+// var Male =characters.filter(x=> x.gender ==='male');
+// console.log(Male);
+
+//5
+
+// var Names= characters.map(x=> x.name);
+// console.log(Names);
+
+//6
+
+// var Hieght= characters.map(x=> x.height);
+// console.log(Hieght);
+
+//7
+
+// var Hieght= characters.sort((a, b)=> a.height - b.height);
+// console.log(Hieght);
+
+// 8
+// var Hieght= characters.sort((a, b)=> b.height - a.height);
+// console.log(Hieght);
+
+//9
+
+// var Mass=characters.every(x=> x.mass>40);
+// console.log(Mass);
+
+//10
+
+// var Short=characters.every(x=> x.height<200);
+// console.log(Short);
+
+//11
+
+// var Blue= characters.some(x=> x.eye_color=="blue");
+// console.log(Blue);
+
+//12
+
+// var tall= characters.some(x=> x.height>210);
+// console.log(tall);
+
+// console.log("--------------------------------------------");
+// console.log("sultan");
+// console.log("--------------------------------------------");
+// console.log("------------------sultan-----------------");
--- ob.js
+++ ob.js
@@ -34,53 +34,87 @@
 //////////////////////////////////////////////////////////
 
 
-characters = [
-    {
-      name: "Luke Skywalker",
-      height: 177,
-      gender: "male",
-      mass: 77,
-      eye_color: "brown",
-    },
-    {
-      name: "Leia Organa",
-      height: 160,
-      gender: "female",
-      mass: 56,
-      eye_color: "blue",
-    },
-    {
-      name: "Han Solo",
-      height: 180,
-      gender: "male",
-      mass: 80,
-      eye_color: "brown",
-    },
-    {
-      name: "Chewie",
-      height: 222,
-      gender: "male",
-      mass: 190,
-      eye_color: "black",
-    },
-    {
-      name: "kevien",
-      height: 106,
-      gender: "male",
-      mass: 32.2,
-      eye_color: "red",
-    },
-  ];
+// characters = [
+//     {
+//       name: "Luke Skywalker",
+//       height: 177,
+//       gender: "male",
+//       mass: 77,
+//       eye_color: "brown",
+//     },
+//     {
+//       name: "Leia Organa",
+//       height: 160,
+//       gender: "female",
+//       mass: 56,
+//       eye_color: "blue",
+//     },
+//     {
+//       name: "Han Solo",
+//       height: 180,
+//       gender: "male",
+//       mass: 80,
+//       eye_color: "brown",
+//     },
+//     {
+//       name: "Chewie",
+//       height: 222,
+//       gender: "male",
+//       mass: 190,
+//       eye_color: "black",
+//     },
+//     {
+//       name: "kevien",
+//       height: 106,
+//       gender: "male",
+//       mass: 32.2,
+//       eye_color: "red",
+//     },
+//   ];
 
-var fname=characters.filter(x=> x.eye_color==="blue");
-console.log(fname[0].name);
+// var fname=characters.filter(x=> x.eye_color==="blue");
+// console.log(fname.mass);
 
 
-var gen=characters.filter(x => x.mass>50);
-console.log(gen[0].gender);
+// var gen=characters.filter(x => x.mass>50);
+// console.log(gen[0].gender);
 
-var height200=characters.filter(x => x.height>200);
-console.log(height200)
+// var height200=characters.filter(x => x.height>200);
+// console.log(height200)
 
-let maleChar=characters.filter(x => x[2] ==="male");
-console.log(maleChar);
+// let maleChar=characters.filter(x => x[2] ==="male");
+// console.log(maleChar);
+
+
+let user=
+{
+  id:1,
+  Name:'ahmad',
+  lastName :'nasser',
+  age : 27,
+  email:'dddddd@gmail.com',
+  address:{
+
+    street:"sultan",
+    buildingNo:17,
+    city:{
+      City:'riyadh',
+      neber:'alazizia'
+    }
+    
+  },
+  sills:['sport', 'problem solving', 'games']
+}
+// console.log(user);
+// console.log(user.Name);
+// user.id = 25;
+// console.log(user.id)
+// delete user.email
+// console.log(user)
+// console.log(user.address);
+// console.log(user.address.city.neber);
+// console.log(user.sills[2]);
+// console.log(user.sills);
+//  user.sills.map(x=>console.log(x , ','));
+
+
